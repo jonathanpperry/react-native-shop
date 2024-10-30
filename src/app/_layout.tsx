da465ce54cdca3ed2b3338f8bd29d4b2e7241fab
Jonathan Perry <jpper@umich.edu>
2024-10-29 20:26:37 -0400

    Implement Auth provider

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,28 +1,28 @@
 import { Stack } from "expo-router";
+import { AuthProvider } from "../providers/AuthProvider";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="(shop)"
-        options={{ headerShown: false, title: "Shop" }}
-      ></Stack.Screen>
-      <Stack.Screen
-        name="categories"
-        options={{ headerShown: true, title: "Categories" }}
-      ></Stack.Screen>
-      <Stack.Screen
-        name="product"
-        options={{ headerShown: true, title: "Product" }}
-      ></Stack.Screen>
-      <Stack.Screen
-        name="cart"
-        options={{ presentation: "modal", title: "Shopping Cart" }}
-      ></Stack.Screen>
-      <Stack.Screen
-        name="auth"
-        options={{ headerShown: true }}
-      ></Stack.Screen>
-    </Stack>
+    <AuthProvider>
+      <Stack>
+        <Stack.Screen
+          name="(shop)"
+          options={{ headerShown: false, title: "Shop" }}
+        />
+        <Stack.Screen
+          name="categories"
+          options={{ headerShown: true, title: "Categories" }}
+        />
+        <Stack.Screen
+          name="product"
+          options={{ headerShown: true, title: "Product" }}
+        />
+        <Stack.Screen
+          name="cart"
+          options={{ presentation: "modal", title: "Shopping Cart" }}
+        />
+        <Stack.Screen name="auth" options={{ headerShown: false }} />
+      </Stack>
+    </AuthProvider>
   );
 }
